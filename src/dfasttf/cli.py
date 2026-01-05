cfe488ffa4f69dbc5598255abb486076c90898bd
Mostafa Farrag <moah.farag@gmail.com>
2026-01-05 11:17:54 +0100

    create cli module

---
+++ src/dfasttf/cli.py
@@ -0,0 +1,66 @@
+import sys
+import argparse
+import dfasttf.cmd
+
+
+def parse_arguments() -> tuple:
+    """
+    Parse the command line arguments.
+
+    Arguments
+    ---------
+    None
+
+    Returns
+    -------
+    config_name : Optional[str]
+        Name of the analysis configuration file.
+    rivers_file : str
+        Name of rivers configuration file.
+    """
+
+    parser = argparse.ArgumentParser(description="D-FAST-RBK")
+
+    parser.add_argument(
+        "--config",
+        default="unspecified",
+        help="name of analysis configuration file ('%(default)s' is default)",
+    )
+
+    parser.add_argument(
+        "--rivers",
+        default="unspecified",
+        help="name of rivers configuration file ('Dutch_rivers_v3.ini' is default)",
+    )
+
+    parser.add_argument(
+        "--ships",
+        default="unspecified",
+        help="name of ship dimensions file ('ship_dimensions.ini' is default)",
+    )
+
+    parser.set_defaults(reduced_output=False)
+    args = parser.parse_args()
+
+    config_file = args.__dict__["config"]
+    rivers_file = args.__dict__["rivers"]
+    ships_file = args.__dict__["ships"]
+    if config_file == "unspecified":
+        config_file = "examples/c01 - Waal/config.ini"
+    if rivers_file == "unspecified":
+        rivers_file = "Dutch_rivers_v3.ini"
+    if ships_file == "unspecified":
+        # TODO: fix this path
+        ships_file = "dfasttf/src/ship_dimensions.ini"
+
+    return config_file, rivers_file, ships_file
+
+def main(argv: list[str] | None = None) -> int:
+    config_file, rivers_file, ships_file = parse_arguments()
+    dfasttf.cmd.run(config_file, ships_file)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
