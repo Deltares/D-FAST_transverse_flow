8628a0bfe98cc0cf70ef33fc649854371b174e0e
Mostafa Farrag <moah.farag@gmail.com>
2026-01-07 12:21:21 +0100

    simplify test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,22 +7,17 @@ Usage:
 """
 import sys
 import subprocess
-from pathlib import Path
-from dfasttf import __path__
 
 
 def test_cli():
     """Test the CLI command with the NVO Maas example."""
     config = "examples/c04-nvo-maas/config.ini"
 
-    ship_dimensions = Path(__path__[0]) / "data" / "ship_dimensions.ini"
-
     cmd = [
         sys.executable,
         "-m",
         "dfasttf",
         "--config", str(config),
-        "--ships", str(ship_dimensions),
     ]
     
     result = subprocess.run(cmd, capture_output=True, text=True)
